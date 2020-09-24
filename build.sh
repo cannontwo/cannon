de9bc18c062352891fa1677cfeb2cf71cb3aece3
Cannon Lewis <cannon10100@gmail.com>
2020-09-24 01:12:29 -0500

    Added code coverage reporting

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DBOOST_ROOT=/usr/include/boost -DBOOST_LIBRARYDIR=/usr/lib/x86_64-linux-gnu ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DBOOST_ROOT=/usr/include/boost -DBOOST_LIBRARYDIR=/usr/lib/x86_64-linux-gnu ..
 make
+make test
+make Cannon_coverage
 cp compile_commands.json ../
 cd ..
-make -C build test
