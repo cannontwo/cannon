7345b6e9b842f9af800daf8fb67d2d94d2bfdb42
Cannon Lewis <cannon10100@gmail.com>
2020-12-01 15:20:10 -0600

    Added other experiments for COMP 509 homework

--- scripts/generate_random_3sat_formulas.cpp
+++ scripts/generate_random_3sat_formulas.cpp
@@ -8,9 +8,25 @@
 using namespace cannon::logic;
 
 int main() {
-  unsigned int num_vars = 150;
+  unsigned int num_vars = 100;
   double clause_ratio = 3.0;
 
+  while (clause_ratio < 6.2) {
+    for (int i = 0; i < 100; i++) {
+      unsigned int l = (unsigned int)std::floor(clause_ratio * num_vars);
+      std::string filename = "formulas/random_3sat/N" + std::to_string(num_vars) +
+        "L" + std::to_string(l) + "_" + std::to_string(i) + ".cnf";
+      std::ofstream os(filename);
+
+      os << write_cnf(generate_random_formula(num_vars, l));
+      os.close();
+    }
+
+    clause_ratio += 0.2;
+  }
+
+  num_vars = 150;
+  clause_ratio = 3.0;
   while (clause_ratio < 6.2) {
     for (int i = 0; i < 100; i++) {
       unsigned int l = (unsigned int)std::floor(clause_ratio * num_vars);
