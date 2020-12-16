67d4a7c8c60e1dc706675a5d145223cdd15d3f71
Cannon Lewis <cannon10100@gmail.com>
2020-12-16 17:10:15 -0600

    Fixed small control bug

--- cannon/physics/systems/holonomic_2d.hpp
+++ cannon/physics/systems/holonomic_2d.hpp
@@ -44,11 +44,14 @@ namespace cannon {
           }
 
           std::pair<VectorXd, double> step(double ux, double uy) {
-            state_[2] = ux;
-            state_[3] = uy;
+            double clipped_ux = std::max(-1.0, std::min(ux, 1.0));
+            double clipped_uy = std::max(-1.0, std::min(uy, 1.0));
+
+            state_[2] = clipped_ux;
+            state_[3] = clipped_uy;
 
             double goal_r = -(state_.head(2) - goal_).norm();
-            double control_r = -(std::abs(ux), + std::abs(uy));
+            double control_r = -(std::abs(clipped_ux), + std::abs(clipped_uy));
 
             double reward = goal_r + 0.1*control_r;
 
