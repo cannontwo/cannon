222100c1e0d39a861de4adb7fd5305fa542bc183
Cannon Lewis <cannon10100@gmail.com>
2021-01-18 18:23:28 -0600

    Documented graphics/character.hpp. Addresses #86

--- cannon/graphics/character.hpp
+++ cannon/graphics/character.hpp
@@ -15,25 +15,56 @@ using namespace Eigen;
 namespace cannon {
   namespace graphics {
 
+    /*!
+     * \brief Class representing a character in a font.
+     *
+     * This class encapsulates the data needed to draw a specific character
+     * loaded from an OpenFont font. It is adapted from
+     * https://learnopengl.com/In-Practice/Text-Rendering .
+     *
+     * \sa cannon::graphics::Font
+     *
+     */
     class Character {
       public:
         Character() = delete;
 
+        /*!
+         * Constructor taking the character that this object represents, the
+         * texture for the character, and various parameters controlling the
+         * character display.
+         *
+         * \param c The character this object represents in the font.
+         * \param t The texture used to draw the character.
+         * \param s Size of the character glyph.
+         * \param b Bearing (offset) of the character glyph.
+         * \param a Advance from this character to the next.
+         */
         Character(char c, std::shared_ptr<Texture> t, Vector2f s, Vector2f b, 
             unsigned int a) : char_(c), texture(t), size(s),
             bearing(b), advance(a) {}
 
+        /*!
+         * Copy constructor.
+         *
+         * \param c The character to copy.
+         */
         Character(Character& c) : char_(c.char_), texture(c.texture),
           size(c.size), bearing(c.bearing), advance(c.advance){}
 
+        /*!
+         * Move constructor.
+         *
+         * \param c The character to move.
+         */
         Character(Character&& c) : char_(c.char_), texture(c.texture),
           size(c.size), bearing(c.bearing), advance(c.advance){}
 
-        char char_;
-        std::shared_ptr<Texture> texture;
-        Vector2f size;
-        Vector2f bearing;
-        unsigned int advance;
+        char char_; //!< The character that this object represents.
+        std::shared_ptr<Texture> texture; //!< The texture used to draw this character.
+        Vector2f size; //!< The size of this character's glyph.
+        Vector2f bearing; //!< The bearing of this character's glyph.
+        unsigned int advance; //!< The advance amount for this character.
     };
 
   } // namespace graphics
