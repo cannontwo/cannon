f26761ab3c4684322f3b37d000c9125203aac0fa
Cannon Lewis <cannon10100@gmail.com>
2021-01-19 19:41:58 -0600

    Documented graphics/cubemap.hpp. Addresses #86

--- cannon/graphics/cubemap.hpp
+++ cannon/graphics/cubemap.hpp
@@ -1,6 +1,11 @@
 #ifndef CANNON_GRAPHICS_CUBEMAP_H
 #define CANNON_GRAPHICS_CUBEMAP_H 
 
+/*!
+ * \file cannon/graphics/cubemap.hpp
+ * File containing Cubemap class definition.
+ */
+
 #include <string>
 #include <stdexcept>
 #include <vector>
@@ -12,10 +17,25 @@
 namespace cannon {
   namespace graphics {
 
+    /*!
+     * \brief Class representing an OpenGL cubemap.
+     *
+     * This class encapsulates the various OpenGL objects and state-changing
+     * functions necessary to use a cubemap (essentially a collection of six
+     * regular textures) in OpenGL. In particular, it can be used for rendering
+     * skyboxes. It is adapted from
+     * https://learnopengl.com/Advanced-OpenGL/Cubemaps .
+     */
     class Cubemap {
       public:
 
-        // Empty cubemap. Does not change OpenGL state
+        /*!
+         * Constructor that makes an empty cubemap. Does not affect OpenGL state.
+         *
+         * \param width The width of the faces of the cubemap.
+         * \param height The height of the faces of the cubemap.
+         * \param channels The number of channels in each face of the cubemap.
+         */
         Cubemap(int width=800, int height=600, int num_channels=3) {
 
           glGenTextures(1, &gl_cubemap_);
@@ -33,6 +53,12 @@ namespace cannon {
           unbind();
         }
 
+        /*!
+         * Constructor that makes a cubemap by loading the passed textures. 
+         *
+         * \param face_paths A vector of six strings that should be parsed as
+         * texture files in order to initialize the faces of the cubemap.
+         */
         Cubemap(std::vector<std::string> face_paths) {
           assert(face_paths.size() == 6);
 
@@ -78,23 +104,61 @@ namespace cannon {
         }
 
 
-        // Sets GL_TEXTURE_CUBE_MAP to gl_cubemap_ and GL_ACTIVE_TEXTURE to
-        // texture_unit
+        /*!
+         * Binds this cubemap for OpenGL drawing.
+         *
+         * Sets GL_TEXTURE_CUBE_MAP to gl_cubemap_ and GL_ACTIVE_TEXTURE to
+         * texture_unit. 
+         *
+         * \param texture_unit The texture unit to bind this cubemap to.
+         */
         void bind(GLenum texture_unit) const;
 
-        // Sets GL_TEXTURE_CUBE_MAP to 0 and GL_ACTIVE_TEXTURE to GL_TEXTURE0
+        /*!
+         * Unbinds this cubemap.
+         *
+         * Sets GL_TEXTURE_CUBE_MAP to 0 and GL_ACTIVE_TEXTURE to GL_TEXTURE0
+         */
         void unbind() const;
 
+        /*!
+         * Set the textures making up the faces of this cubemap to repeat on
+         * wrap.
+         */
         void set_wrap_repeat();
+
+        /*!
+         * Set the textures making up the faces of this cubemap to mirror on
+         * wrap.
+         */
         void set_wrap_mirrored_repeat();
+
+        /*!
+         * Set the textures making up the faces of this cubemap to clamp to the
+         * edge on wrap.
+         */
         void set_wrap_clamp_edge();
+
+        /*!
+         * Set the textures making up the faces of this cubemap to clamp to a
+         * border color on wrap.
+         */
         void set_wrap_clamp_border();
 
+        /*!
+         * Set the textures making up the faces of this cubemap to use linear
+         * filtering.
+         */
         void set_filter_linear();
+
+        /*!
+         * Set the textures making up the faces of this cubemap to use nearest
+         * value filtering.
+         */
         void set_filter_nearest();
 
       private:
-        unsigned int gl_cubemap_;
+        unsigned int gl_cubemap_; //!< OpenGL object identifier for this cubemap.
 
     };
 
