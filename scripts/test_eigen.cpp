683804287eb925a457cef36d46535c77fbf93713
Cannon Lewis <cannon10100@gmail.com>
2021-02-03 16:09:48 -0600

    Working on research

--- scripts/test_eigen.cpp
+++ scripts/test_eigen.cpp
@@ -20,4 +20,16 @@ int main() {
   log_info(x.array() > 0.0);
   log_info((x.array() <= y.array()).all());
   log_info(x.array().max(1));
+
+  Matrix3d A;
+  A << 1, 2, 3,
+       4, 5, 6,
+       7, 8, 9;
+
+  Vector3d a = Vector3d::Zero();
+  for (unsigned int i = 0; i < 3; i++) {
+    a[i] = *(A.row(1).data() + i);
+  }
+  log_info(a);
+
 }
