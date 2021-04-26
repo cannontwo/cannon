793ae5035c1d89ce01cf93a8bf771642f338a1d2
Cannon Lewis <cannon10100@gmail.com>
2021-04-26 10:35:31 -0500

    Documented graphics/point_light.hpp. Partially addresses #86.

--- cannon/graphics/point_light.hpp
+++ cannon/graphics/point_light.hpp
@@ -1,6 +1,11 @@
 #ifndef CANNON_GRAPHICS_POINT_LIGHT_H
 #define CANNON_GRAPHICS_POINT_LIGHT_H
 
+/*!
+ * \file cannon/graphics/point_light.hpp
+ * File containing PointLight class definition.
+ */
+
 #include <memory>
 
 #include <cannon/graphics/light.hpp>
@@ -9,19 +14,40 @@
 namespace cannon {
   namespace graphics {
 
+    /*!
+     * \brief Class representing a point light. 
+     *
+     * This class embodies the behavior of a point light, which radiates light
+     * in all directions. In particular, such a light has ambient, diffuse, and
+     * specular colors, as well as a position from which it radiates. The
+     * precise behavior of this light is controlled by the shaders used. The
+     * position of the point light is represented by a cube.
+     */
     class PointLight : public Light {
       public:
         PointLight() = delete;
 
+        /*!
+         * Constructor taking an ambient color, diffuse color, specular color,
+         * and cube geometry to represent the light's position.
+         */
         PointLight(Vector4f ambient, Vector4f diffuse, Vector4f specular,
             std::shared_ptr<geometry::DrawableGeom> cube) : Light(ambient,
             diffuse, specular), cube_(cube) {}
 
+        /*!
+         * Constructor taking an ambient color, diffuse color, specular color,
+         * cube geometry, and scalar parameters controlling the light's falloff
+         * behavior.
+         */
         PointLight(Vector4f ambient, Vector4f diffuse, Vector4f specular,
             std::shared_ptr<geometry::DrawableGeom> cube, float constant, float linear, float quadratic) :
           Light(ambient, diffuse, specular), constant_(constant),
           linear_(linear), quadratic_(quadratic), cube_(cube) {}
 
+        /*!
+         * Method inherited from Light. Does not change OpenGL state.
+         */
         virtual void apply(const std::shared_ptr<geometry::DrawableGeom> geom) const override {
           geom->program->set_uniform("light.ambient", (Vector4f)(ambient_ * std::pow(2.0, intensity_)));
           geom->program->set_uniform("light.diffuse", (Vector4f)(diffuse_ * std::pow(2.0, intensity_)));
@@ -33,10 +59,25 @@ namespace cannon {
           geom->program->set_uniform("light.quadratic", quadratic_);
         }
 
+        /*!
+         * Variant of the apply method which is useful when this light is one of a collection. 
+         *
+         * \param geom The geometry to apply this light to.
+         * \param idx The index of this light, controlling which shader uniforms are set.
+         */
         void apply(const std::shared_ptr<geometry::DrawableGeom> geom, int idx) const {
           apply(geom->program, idx);
         }
 
+        /*!
+         * Variant of the apply method which is useful when this light is one
+         * of a collection being applied to a shader program.
+         *
+         * This function does not change OpenGL state.
+         *
+         * \param s The shader program to apply this light to.
+         * \param idx The index of this light, controlling which shader uniforms are set.
+         */
         void apply(const std::shared_ptr<ShaderProgram> s, int idx) const {
           std::string preamble("point_lights[");
           preamble += std::to_string(idx);
@@ -52,6 +93,12 @@ namespace cannon {
           s->set_uniform(preamble + ".quadratic", quadratic_);
         }
 
+        /*!
+         * Method to write ImGui controls affecting this light's parameters.
+         *
+         * \param idx The index of this light in a collection of lights, used
+         * to disambiguate the corresponding controls.
+         */
         virtual void write_imgui(int idx) {
           if (ImGui::TreeNode((name_ + " " + std::to_string(idx)).c_str())) {
             ImGui::ColorEdit3("color", specular_.data());
@@ -72,12 +119,12 @@ namespace cannon {
         }
 
       private:
-        float constant_ = 1.0;
-        float linear_ = 0.14;
-        float quadratic_ = 0.07;
+        float constant_ = 1.0; //!< Constant term in light intensity falloff
+        float linear_ = 0.14; //!< Linear term in light intensity falloff
+        float quadratic_ = 0.07; //!< Quadratic term in light intensity falloff
 
-        std::shared_ptr<geometry::DrawableGeom> cube_;
-        std::string name_ = std::string("Point Light");
+        std::shared_ptr<geometry::DrawableGeom> cube_; //!< Cube geometry for this light
+        std::string name_ = std::string("Point Light"); //!< Name for this light
     };
 
   } // namespace graphics
