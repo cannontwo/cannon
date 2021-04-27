2c4b47b63758de030d379afb73bb314452fb21b5
Cannon Lewis <cannon10100@gmail.com>
2021-04-27 11:01:16 -0500

    Document raytracing stuff

--- cannon/ray/ray.hpp
+++ cannon/ray/ray.hpp
@@ -1,5 +1,11 @@
+#pragma once
 #ifndef CANNON_RAY_RAY_H
 #define CANNON_RAY_RAY_H 
+
+/*!
+ * \file cannon/ray/ray.hpp
+ * File containing Ray class definition.
+ */
 
 #include <Eigen/Dense>
 
@@ -8,17 +14,34 @@ using namespace Eigen;
 namespace cannon {
   namespace ray {
     
+    /*!
+     * \brief Class representing a ray for raytracing.
+     */
     class Ray {
       public:
+        /*!
+         * Default constructor.
+         */
         Ray() {}
 
+        /*!
+         * Constructor taking an origin and direction for this ray.
+         */
         Ray(const Vector3d& origin, const Vector3d& direction) : orig_(origin), dir_(direction) {}
 
+        /*!
+         * Method computing the location in 3D space that is the input distance
+         * along this ray.
+         *
+         * \param t Distance along ray to compute.
+         *
+         * \returns Point at distance t along this ray.
+         */
         Vector3d at(double t) const;
 
       public:
-        Vector3d orig_;
-        Vector3d dir_;
+        Vector3d orig_; //!< Origin of this ray.
+        Vector3d dir_; //!< Direction of this ray.
 
     };
 
--- cannon/ray/write_ppm.hpp
+++ cannon/ray/write_ppm.hpp
@@ -1,5 +1,11 @@
+#pragma once
 #ifndef CANNON_RAY_WRITE_PPM
 #define CANNON_RAY_WRITE_PPM
+
+/*!
+ * \file cannon/ray/write_ppm.hpp
+ * File containing utility functions for working with PPM files.
+ */
 
 #include <iostream>
 
@@ -10,6 +16,12 @@ using namespace Eigen;
 namespace cannon {
   namespace ray {
 
+    /*!
+     * Function to write a color in PPM format to the input stream.
+     *
+     * \param os The stream to write this color to.
+     * \param pixel_color The RGB color to write in PPM format.
+     */
     void write_color(std::ostream& os, const Vector3d& pixel_color);
 
   }
