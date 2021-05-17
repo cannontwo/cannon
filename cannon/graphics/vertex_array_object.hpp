885df5173eba4c53755558b5ebc2554d3ee40575
Cannon Lewis <cannon10100@gmail.com>
2021-05-17 10:10:30 -0500

    Documented cannon/graphics/vertex_array_object.hpp. Partially addresses #86

--- cannon/graphics/vertex_array_object.hpp
+++ cannon/graphics/vertex_array_object.hpp
@@ -2,6 +2,11 @@
 #ifndef CANNON_GRAPHICS_VERTEX_ARRAY_OBJECT_H
 #define CANNON_GRAPHICS_VERTEX_ARRAY_OBJECT_H 
 
+/*!
+ * \file cannon/graphics/vertex_array_object.hpp
+ * \brief File containing VertexArrayObject class definition.
+ */
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -9,21 +14,39 @@
 namespace cannon {
   namespace graphics {
 
+    /*!
+     * \brief Class encapsulating an OpenGL Vertex Array Object. Responsible
+     * for binding and unbinding the encapsulated OpenGL object. Note that this
+     * class cannot be copied because there would be harmful interactions
+     * between multiple VertexArrayObjects with the same associated OpenGL
+     * object.
+     */
     class VertexArrayObject {
       public:
-        // Does not change OpenGL state
+
+        /*!
+         * Default Constructor.
+         *
+         * Does not change OpenGL state.
+         */
         VertexArrayObject() {
           glGenVertexArrays(1, &gl_vertex_array_object_);
         }
 
         VertexArrayObject(VertexArrayObject& vao) = delete;
 
+        /*!
+         * Move constructor.
+         */
         VertexArrayObject(VertexArrayObject&& vao) :
           gl_vertex_array_object_(vao.gl_vertex_array_object_),
           declared_vertex_attribs_(vao.declared_vertex_attribs_) {
             vao.gl_vertex_array_object_ = -1;
         } 
 
+        /*!
+         * Destructor, which frees OpenGL resources if possible.
+         */
         ~VertexArrayObject() {
           if (glfwGetCurrentContext() != NULL) {
             unbind();
@@ -31,22 +54,41 @@ namespace cannon {
           }
         }
 
-        // Sets GL_VERTEX_ARRAY_BINDING to gl_vertex_array_object_
+        /*!
+         * Method to bind the associated OpenGL Vertex Array Object.
+         *
+         * Sets GL_VERTEX_ARRAY_BINDING to gl_vertex_array_object_.
+         */
         void bind();
 
-        // Sets GL_VERTEX_ARRAY_BINDING to 0
+        /*!
+         * Method to unbind the associated OpenGL Vertex Array Object.
+         *
+         * Sets GL_VERTEX_ARRAY_BINDING to 0.
+         */
         void unbind();
 
-        // Does not affect OpenGL state
+        /*!
+         * Method to get next vertex attribute number for use with vertex buffers.
+         *
+         * Does not affect OpenGL state.
+         *
+         * \returns Next unallocated vertex attribute number.
+         * 
+         * \sa cannon::graphics::VertexBuffer
+         */
         int get_next_vertex_attribute_num();
 
         friend std::ostream& operator<<(std::ostream&, const VertexArrayObject&);
 
       private:
-        unsigned int gl_vertex_array_object_;
-        int declared_vertex_attribs_ = 0;
+        unsigned int gl_vertex_array_object_; //!< OpenGL vertex array object
+        int declared_vertex_attribs_ = 0; //!< Number of already allocated vertex attributes for this VAO.
     };
 
+    /*!
+     * Operator implementation for debug printing.
+     */
     std::ostream& operator<<(std::ostream&, const VertexArrayObject&);
 
   } // namespace graphics
