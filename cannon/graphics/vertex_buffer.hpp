d714dafa404208840c50d3340acd89fd0da45f71
Cannon Lewis <cannon10100@gmail.com>
2021-05-18 09:16:30 -0500

    Documented graphics/vertex_array_object.hpp. Partially addresses #86

--- cannon/graphics/vertex_buffer.hpp
+++ cannon/graphics/vertex_buffer.hpp
@@ -2,6 +2,11 @@
 #ifndef CANNON_GRAPHICS_VERTEX_BUFFER_H
 #define CANNON_GRAPHICS_VERTEX_BUFFER_H 
 
+/*!
+ * \file cannon/graphics/vertex_buffer.hpp
+ * \brief Class containing VertexBuffer class definition.
+ */
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <Eigen/Dense>
@@ -16,14 +21,30 @@ namespace cannon {
 
     CANNON_CLASS_FORWARD(VertexArrayObject);
 
+    /*!
+     * \brief Class encapsulating an OpenGL vertex buffer object. Handles and
+     * represents state of the buffer.
+     */
     class VertexBuffer {
       public:
+
+        /*!
+         * \brief Default constructor.
+         */
         VertexBuffer() : vao_(nullptr) {}
 
+        /*!
+         * \brief Constructor taking a VertexArrayObject that this VertexBuffer
+         * will be associated with. This VertexBuffer will have a vertex
+         * attribute number allocated by the input VAO.
+         */
         VertexBuffer(VertexArrayObjectPtr vao);
 
         VertexBuffer(VertexBuffer& buf) = delete;
 
+        /*!
+         * \brief Move constructor. 
+         */
         VertexBuffer(VertexBuffer&& buf) :
           gl_vertex_buffer_object_(buf.gl_vertex_buffer_object_),
           gl_vertex_attribute_num_(buf.gl_vertex_attribute_num_),
@@ -31,6 +52,9 @@ namespace cannon {
             buf.gl_vertex_buffer_object_ = -1;
           }
 
+        /*!
+         * \brief Destructor. Cleans up OpenGL vertex buffer object if possible.
+         */
         ~VertexBuffer() {
           if (glfwGetCurrentContext() != NULL) {
             if (vao_ != nullptr)
@@ -42,42 +66,92 @@ namespace cannon {
           }
         }
 
-        // Does not affect OpenGL State
+        /*!
+         * \brief Method to initialize this vertex buffer with a VAO.
+         *
+         * Does not affect OpenGL State
+         *
+         * \param vao The VertexArrayObject to associate this vertex buffer with.
+         */
         void init(VertexArrayObjectPtr vao);
 
-        // Sets GL_VERTEX_ARRAY_BINDING to vao_->gl_vertex_array_object_ and
-        // GL_ARRAY_BUFFER_BINDING to gl_vertex_buffer_object
+        /*!
+         * \brief Method to bind this verex buffer for drawing. 
+         *
+         * Sets GL_VERTEX_ARRAY_BINDING to vao_->gl_vertex_array_object_ and
+         * GL_ARRAY_BUFFER_BINDING to gl_vertex_buffer_object
+         */
         void bind() const;
 
-        // Sets GL_VERTEX_ARRAY_BINDING to 0 and GL_ARRAY_BUFFER_BINDING to 0
+        /*!
+         * \brief Method to unbind this vertex buffer.
+         *
+         * Sets GL_VERTEX_ARRAY_BINDING to 0 and GL_ARRAY_BUFFER_BINDING to 0
+         */
         void unbind() const;
 
-        // Does not affect OpenGL state
+        /*!
+         * \brief Method to add data to this vertex buffer. 
+         *
+         * Does not affect OpenGL state
+         */
         void buffer(MatrixX2f vertices);
 
-        // Does not affect OpenGL state
+        /*!
+         * \brief Method to add data to this vertex buffer. 
+         *
+         * Does not affect OpenGL state
+         */
         void buffer(MatrixX3f vertices);
 
-        // Does not affect OpenGL state
+        /*!
+         * \brief Method to add data to this vertex buffer. 
+         *
+         * Does not affect OpenGL state
+         */
         void buffer(MatrixX4f vertices);
 
-        // Does not affect OpenGL state
+        /*!
+         * \brief Method to replace the data in this vertex buffer. Can be more
+         * efficient than buffer() if the new data has the same size as the old
+         * data.
+         *
+         * Does not affect OpenGL state
+         */
         void replace(MatrixX2f vertices);
 
-        // Does not affect OpenGL state
+        /*!
+         * \brief Method to replace the data in this vertex buffer. Can be more
+         * efficient than buffer() if the new data has the same size as the old
+         * data.
+         *
+         * Does not affect OpenGL state
+         */
         void replace(MatrixX3f vertices);
 
-        // Does not affect OpenGL state
+        /*!
+         * \brief Method to replace the data in this vertex buffer. Can be more
+         * efficient than buffer() if the new data has the same size as the old
+         * data.
+         *
+         * Does not affect OpenGL state
+         */
         void replace(MatrixX4f vertices);
 
         friend std::ostream& operator<<(std::ostream&, const VertexBuffer&);
 
       private:
-        void set_vertex_attribute_pointer(int dim);
 
-        unsigned int gl_vertex_buffer_object_;
-        int gl_vertex_attribute_num_;
-        VertexArrayObjectPtr vao_;
+        /*!
+         * \brief Method to set the vertex attribute number for this vertex buffer.
+         *
+         * \param dim Dimension of vertices in this vertex buffer.
+         */
+        void set_vertex_attribute_pointer(int dim); 
+
+        unsigned int gl_vertex_buffer_object_; //!< OpenGL object managed by this class
+        int gl_vertex_attribute_num_; //!< Vertex attribute number for this vertex buffer
+        VertexArrayObjectPtr vao_; //!< VAO associated with this buffer
     };
 
     std::ostream& operator<<(std::ostream&, const VertexBuffer&);
