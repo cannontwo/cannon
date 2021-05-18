084ede5200cede134cfe8c2221e7753d1dd2e6fc
Cannon Lewis <cannon10100@gmail.com>
2021-05-18 12:04:31 -0500

    Implemented simple utility to generate lattice points

---
+++ cannon/math/lattice_points.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#ifndef CANNON_MATH_LATTICE_POINTS_H
+#define CANNON_MATH_LATTICE_POINTS_H 
+
+#include <queue>
+
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+namespace cannon {
+  namespace math {
+
+      using VectorXu = Matrix<unsigned int, Dynamic, 1>;
+
+      /*!
+       * \brief Generate all lattice points on an integer-valued grid.
+       *
+       * \param dim Which dimension of the lattice to generate.
+       * \param sizes Number of elements in each dimension of the grid.
+       *
+       * \return Lattice points for dimensions 0 through dim-1
+       */
+      std::vector<VectorXu> make_lattice_points(unsigned int dim, const VectorXu& sizes) {
+        std::vector<VectorXu> ret_vec;
+
+        if (dim == 1) {
+          for (unsigned int i = 0; i < sizes[dim-1]; i++) {
+            VectorXu tmp = VectorXu::Zero(dim);
+            tmp[0] = i;
+            ret_vec.push_back(tmp);
+          }
+        } else {
+          for (unsigned int i = 0; i < sizes[dim-1]; i++) {
+            auto smaller_vec = make_lattice_points(dim-1, sizes);
+            for (const auto& coord : smaller_vec) {
+              VectorXu tmp = VectorXu::Zero(dim);
+              tmp.head(dim-1) = coord;
+              tmp[dim-1] = i;
+
+              ret_vec.push_back(tmp);
+            }
+          }
+        }
+
+        return ret_vec;
+      }
+
+      /*!
+       * \brief Generate all lattice points on an integer-valued grid.
+       *
+       * \param sizes Number of elements in each dimension of the grid.
+       *
+       * \return Lattice points for the grid.
+       */
+      std::vector<VectorXu> make_lattice_points(const VectorXu& sizes) {
+        return make_lattice_points(sizes.size(), sizes);
+      }
+
+  } // namespace math
+} // namespace cannon
+
+
+#endif /* ifndef CANNON_MATH_LATTICE_POINTS_H */
---
+++ cannon/math/lattice_points.test.cpp
@@ -0,0 +1,47 @@
+#include <catch2/catch.hpp>
+
+#include <cannon/math/lattice_points.hpp>
+#include <cannon/log/registry.hpp>
+
+using namespace cannon::math;
+using namespace cannon::log;
+
+TEST_CASE("LatticePoints", "[math]") {
+  VectorXu one_d_sizes(1);
+  one_d_sizes << 5;
+
+  auto points = make_lattice_points(one_d_sizes);
+
+  for (auto& p : points) {
+    log_info(p);
+  }
+
+  REQUIRE(points.size() == 5);
+  for (int i = 0; i < 5; i++) {
+    VectorXu tmp = VectorXu::Zero(1);
+    tmp[0] = i;
+
+    REQUIRE(std::find(points.begin(), points.end(), tmp) != points.end());
+  }
+
+  VectorXu two_d_sizes(2);
+  two_d_sizes << 5,
+                 5;
+
+  points = make_lattice_points(two_d_sizes);
+
+  for (auto& p : points) {
+    log_info(p);
+  }
+
+  REQUIRE(points.size() == 25);
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      VectorXu tmp = VectorXu::Zero(2);
+      tmp[0] = i;
+      tmp[1] = j;
+
+      REQUIRE(std::find(points.begin(), points.end(), tmp) != points.end());
+    }
+  }
+}
