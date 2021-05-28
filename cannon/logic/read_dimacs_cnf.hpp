905d8879a89d058e9923b098468eaa7a8ee53300
Cannon Lewis <cannon10100@gmail.com>
2021-05-28 12:28:10 -0500

    Finished documenting logic package

--- cannon/logic/read_dimacs_cnf.hpp
+++ cannon/logic/read_dimacs_cnf.hpp
@@ -1,6 +1,11 @@
 #ifndef CANNON_LOGIC_READ_DIMACS_CNF_H
 #define CANNON_LOGIC_READ_DIMACS_CNF_H 
 
+/*!
+ * \file cannon/logic/read_dimacs_cnf.hpp
+ * \brief File containing utilities for parsing formulas from DIMACS files.
+ */
+
 #include <string>
 
 #include <cannon/utils/class_forward.hpp>
@@ -11,11 +16,41 @@ namespace cannon {
     CANNON_CLASS_FORWARD(CNFFormula);
     CANNON_CLASS_FORWARD(Clause);
 
-    // Parses a DIMACS specification for a CNF formula
+    /*!
+     * \brief Parse a conjunctive normal form formula from the input DIMACS
+     * string. See https://people.sc.fsu.edu/~jburkardt/data/cnf/cnf.html
+     *
+     * \param s The string to parse
+     *
+     * \returns The parsed CNF formula.
+     */
     CNFFormula parse_cnf(const std::string& s);
+
+    /*!
+     * \brief Parse a CNF formula from the input DIMACS-formatted file.
+     *
+     * \param path The file path to read.
+     *
+     * \returns The parsed CNF formula.
+     */
     CNFFormula load_cnf(const std::string& path);
 
+    /*!
+     * \brief Parse a single clause from the input DIMACS string.
+     *
+     * \param s String to parse a clause from. Will be modified.
+     *
+     * \returns The parsed clause.
+     */
     Clause parse_clause(std::string& s);
+    
+    /*!
+     * \brief Parse a single clause from the input DIMACS string.
+     *
+     * \param s String to parse a clause from. 
+     *
+     * \returns The parsed clause.
+     */
     Clause parse_clause(const std::string& s);
 
   } // namespace logic
--- cannon/logic/write_dimacs_cnf.hpp
+++ cannon/logic/write_dimacs_cnf.hpp
@@ -1,6 +1,11 @@
 #ifndef CANNON_LOGIC_WRITE_DIMACS_CNF_H_
 #define CANNON_LOGIC_WRITE_DIMACS_CNF_H_ 
 
+/*!
+ * \file cannon/logic/write_dimacs_cnf.hpp
+ * \brief File containing utilities for writing a CNF formula to DIMACS format.
+ */
+
 #include <string>
 
 #include <cannon/utils/class_forward.hpp>
@@ -10,6 +15,13 @@ namespace cannon {
 
     CANNON_CLASS_FORWARD(CNFFormula);
 
+    /*!
+     * \brief Write a CNF formula to a DIMACS-formatted string.
+     *
+     * \param f The formula to write to string.
+     *
+     * \returns The DIMACS string.
+     */
     std::string write_cnf(const CNFFormula& f);
 
   } // namespace logic
