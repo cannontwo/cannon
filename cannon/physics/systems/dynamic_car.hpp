8993910045a08f5e61c3809feec607727d6e78d4
Cannon Lewis <cannon10100@gmail.com>
2021-06-02 15:08:11 -0500

    Fixed dynamic car. Resolves #76

--- cannon/physics/systems/dynamic_car.hpp
+++ cannon/physics/systems/dynamic_car.hpp
@@ -1,5 +1,5 @@
-#ifndef CANNON_PHYSICS_SYSTEMS_KINEMATIC_CAR_H
-#define CANNON_PHYSICS_SYSTEMS_KINEMATIC_CAR_H 
+#ifndef CANNON_PHYSICS_SYSTEMS_DYNAMIC_CAR_H
+#define CANNON_PHYSICS_SYSTEMS_DYNAMIC_CAR_H 
 
 #include <random>
 #include <cassert>
@@ -27,22 +27,24 @@ namespace cannon {
   namespace physics {
     namespace systems {
 
-      struct CAKinCarSystem : System {
-        CAKinCarSystem(double l = 1.0) : l_(l) {}
+      struct DynamicCarSystem : System {
+        DynamicCarSystem(double l = 1.0) : l_(l) {}
 
         virtual void operator()(const VectorXd& s, VectorXd& dsdt, const double /*t*/) override {
           double th = s[2];
           double v = s[3];
-          double ua = s[4];
-          double uth = s[5];
+          double dth = s[4];
+          double ua = s[5];
+          double uth = s[6];
 
-          dsdt.resize(6);
+          dsdt.resize(7);
           dsdt[0] = v * std::cos(th);
           dsdt[1] = v * std::sin(th);
-          dsdt[2] = (v / l_) * uth;
+          dsdt[2] = (v / l_) * dth;
           dsdt[3] = ua;
-          dsdt[4] = 0.0;
+          dsdt[4] = uth;
           dsdt[5] = 0.0;
+          dsdt[6] = 0.0;
         }
 
         virtual void ompl_ode_adaptor(const oc::ODESolver::StateType& q, 
@@ -51,14 +53,15 @@ namespace cannon {
           const double ua = control->as<oc::RealVectorControlSpace::ControlType>()->values[0];
           const double uth = control->as<oc::RealVectorControlSpace::ControlType>()->values[1];
 
-          VectorXd s(5);
+          VectorXd s(7);
           s[0] = q[0];
           s[1] = q[1];
           s[2] = q[2];
           s[3] = q[3];
-          s[4] = ua;
-          s[5] = uth;
-          VectorXd dsdt(5);
+          s[4] = q[4];
+          s[5] = ua;
+          s[6] = uth;
+          VectorXd dsdt(7);
 
           (*this)(s, dsdt, 0.0);
 
@@ -69,33 +72,39 @@ namespace cannon {
         }
 
         virtual std::tuple<MatrixXd, MatrixXd, VectorXd> get_linearization(const VectorXd& x) override {
-          assert(x.size() == 4);
+          assert(x.size() == 5);
 
           double th = x[2];
           double v = x[3];
+          double dth = x[4];
 
           // TODO Don't hardcode timestep at some point
           double timestep = 0.01;
 
-          MatrixXd A(4, 4);
-          A << 1, 0, -v * std::sin(th) * timestep, std::cos(th) * timestep,
-               0, 1, v * std::cos(th) * timestep, std::sin(th) * timestep,
-               0, 0, 1, 0,
-               0, 0, 0, 1;
+          MatrixXd A(5, 5);
+          A << 1, 0, -v * std::sin(th) * timestep, std::cos(th) * timestep, 0,
+               0, 1, v * std::cos(th) * timestep, std::sin(th) * timestep, 0,
+               0, 0, 1, (dth / l_) * timestep, (v / l_) * timestep,
+               0, 0, 0, 1, 0,
+               0, 0, 0, 0, 1;
 
-          MatrixXd B(4, 2);
+          MatrixXd B(5, 2);
           B << 0, 0,
                0, 0,
-               0, (v * timestep) / l_,
-               timestep, 0;
+               0, 0,
+               timestep, 0,
+               0, timestep;
+
           
-          VectorXd c(4); 
-           c << x[0] + v * std::cos(th) * timestep,
-                x[1] + v * std::cos(th) * timestep,
-                th,
-                v;
-          
-          return std::make_tuple(A, B, c);
+          VectorXd c(5); 
+          c << x[0] + v * std::cos(th) * timestep,
+               x[1] + v * std::cos(th) * timestep,
+               th + (v / l_) * dth * timestep,
+               v,
+               dth;
+
+          // We linearize around u = 0, so no additional term is subtracted from c
+          return std::make_tuple(A, B, c - A * x);
         }
 
         static void ompl_post_integration(const ob::State* /*state*/, const
@@ -111,18 +120,18 @@ namespace cannon {
         double l_;
       };
 
-      class ControlAffineKinematicCar {
+      class DynamicCar {
         public:
-          ControlAffineKinematicCar() = delete;
+          DynamicCar() = delete;
 
-          ControlAffineKinematicCar(Vector3d s, Vector3d g) : e_(s_, 6, time_step), start_(s), goal_(g) {
+          DynamicCar(Vector3d s, Vector3d g) : e_(s_, 7, time_step), start_(s), goal_(g) {
             std::random_device rd;
             gen_ = std::mt19937(rd());  
 
             xy_dis_ = std::uniform_real_distribution<double>(-1.0, 1.0);
             th_dis_ = std::uniform_real_distribution<double>(-M_PI, M_PI);
 
-            state_ = VectorXd::Zero(6);
+            state_ = VectorXd::Zero(7);
             reset();
           }
 
@@ -130,8 +139,8 @@ namespace cannon {
             double clipped_ua = std::max(-1.0, std::min(ua, 1.0));
             //double clipped_uth = std::max(-M_PI, std::min(uth, M_PI));
             
-            state_[4] = clipped_ua;
-            state_[5] = uth;
+            state_[5] = clipped_ua;
+            state_[6] = uth;
 
             double goal_r = -std::pow((state_.head(2) - goal_.head(2)).norm(), 2.0);
             double control_r = -std::pow((std::abs(clipped_ua) + std::abs(uth)), 2.0);
@@ -140,7 +149,7 @@ namespace cannon {
             e_.set_state(state_);
             state_ = e_.step();
 
-            return std::make_pair(state_.head(4), reward);
+            return std::make_pair(state_.head(5), reward);
           }
           
           VectorXd reset() {
@@ -148,33 +157,34 @@ namespace cannon {
             state_[1] = xy_dis_(gen_);
             state_[2] = th_dis_(gen_);
             state_[3] = 0.0;
+            state_[4] = 0.0;
             
             state_[4] = 0.0;
             state_[5] = 0.0;
 
-            return state_.head(4);
+            return state_.head(5);
           }
 
+          
           VectorXd reset(const VectorXd& s) {
-            assert(s.size() == 4);
-
+            assert(s.size() == 5);
 
             state_[0] = s[0];
             state_[1] = s[1];
             state_[2] = s[2];
             state_[3] = s[3];
+            state_[4] = s[4];
             
-            state_[4] = 0.0;
             state_[5] = 0.0;
+            state_[6] = 0.0;
 
-            return state_.head(4);
-
+            return state_.head(5);
           }
 
           // In seconds
           const double time_step = 0.01;
 
-          CAKinCarSystem s_;
+          DynamicCarSystem s_;
           
         private:
           RK4Integrator e_;
@@ -194,4 +204,4 @@ namespace cannon {
   } // namespace physics
 } // namespace cannon
 
-#endif /* ifndef CANNON_PHYSICS_SYSTEMS_KINEMATIC_CAR_H */
+#endif /* ifndef CANNON_PHYSICS_SYSTEMS_DYNAMIC_CAR_H */
