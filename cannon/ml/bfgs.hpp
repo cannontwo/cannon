0f45c7fd5b26d0ae46949598fdc86226462ef821
Cannon Lewis <cannon10100@gmail.com>
2021-06-07 13:02:00 -0500

    Documented ml/bfgs.hpp

--- cannon/ml/bfgs.hpp
+++ cannon/ml/bfgs.hpp
@@ -1,6 +1,11 @@
 #ifndef CANNON_ML_BFGS_H
 #define CANNON_ML_BFGS_H 
 
+/*!
+ * \file cannon/ml/bfgs.hpp
+ * \brief File containing BFGSOptimizer class definition.
+ */
+
 #include <functional>
 
 #include <Eigen/Dense>
@@ -17,18 +22,37 @@ namespace cannon {
 
     CANNON_CLASS_FORWARD(OptimizationResult);
 
+    /*!
+     * \brief Class representing a BFGS optimizer, which is a quasi-Newton
+     * unconstrained optimization method for nonlinear problems. See
+     * https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm
+     */
     class BFGSOptimizer {
       public:
         BFGSOptimizer() = delete;
 
+        /*!
+         * \brief Constructor taking a function to optimize and a function
+         * providing gradients for that function.
+         */
         BFGSOptimizer(RealFunc f, MultiFunc f_grad) : f_(f), f_grad_(f_grad) {}
 
+        /*!
+         * \brief Minimize the function stored by this object, beginning from
+         * the input start state.
+         *
+         * \param start Initial state for optimization
+         * \param eps Small number used to detect convergence
+         * \param iterations Maximum number of iterations to perform
+         *
+         * \returns The result of the optimization.
+         */ 
         OptimizationResult optimize(const VectorXd& start, double eps=1e-4, unsigned
             int iterations=100);
 
       private:
-        RealFunc f_;
-        MultiFunc f_grad_;
+        RealFunc f_; //!< The function to minimize
+        MultiFunc f_grad_; //!< Gradient function for the function to minimize
 
     };
 
