c8fb68ae89a87835e43c3d7fc8e6e0401a33e6ad
Cannon Lewis <cannon10100@gmail.com>
2021-06-07 13:11:22 -0500

    Documented ml/optimization_result.hpp (#86)

--- cannon/ml/optimization_result.hpp
+++ cannon/ml/optimization_result.hpp
@@ -1,6 +1,11 @@
 #ifndef CANNON_ML_OPTIMIZATION_RESULT_H
 #define CANNON_ML_OPTIMIZATION_RESULT_H 
 
+/*!
+ * \file cannon/ml/optimization_result.hpp
+ * \brief File containing OptimizationResult class definition.
+ */
+
 #include <Eigen/Dense>
 
 using namespace Eigen;
@@ -8,18 +13,24 @@ using namespace Eigen;
 namespace cannon {
   namespace ml {
 
+    /*!
+     * \brief Class representing the result of an optimization attempt.
+     */
     class OptimizationResult {
       public:
         OptimizationResult() = delete;
 
+        /*!
+         * \brief Constructor initializing all member variables.
+         */
         OptimizationResult(double objective, const VectorXd& solution, 
             unsigned int iterations, bool max_iterations) : objective(objective),
         solution(solution), iterations(iterations), max_iterations(max_iterations) {}
 
-        double objective;
-        VectorXd solution;
-        unsigned int iterations;
-        bool max_iterations;
+        double objective; //!< Value of objective at solution, if found
+        VectorXd solution; //!< Solution state, if found
+        unsigned int iterations; //!< Number of iterations executed while optimizing
+        bool max_iterations; //!< Whether the maximum number of iterations was exceeded
     };
 
     std::ostream& operator<<(std::ostream& os, OptimizationResult result);
