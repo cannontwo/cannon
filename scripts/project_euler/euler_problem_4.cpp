2fa15710667dff0da4c9d6876738cfff5f70b66e
Cannon Lewis <cannon10100@gmail.com>
2021-06-10 11:21:58 -0500

    Solved Project Euler problem 4. (#111)

---
+++ scripts/project_euler/euler_problem_4.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+
+#include <cannon/log/registry.hpp>
+
+using namespace cannon::log;
+
+/*!
+ * A palindromic number reads the same both ways. The largest palindrome made
+ * from the product of two 2-digit numbers is 9009 = 91 x 99. 
+ *
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+bool is_palindrome(unsigned int x) {
+  unsigned int reversed = 0;
+  unsigned int tmp = x;
+
+  unsigned int digits = std::floor(std::log10(x));
+
+  for (unsigned int i = 0; i <= digits; i++) {
+    unsigned int digit = tmp % 10;
+    reversed += std::pow(10, digits - i) * digit;
+    tmp /= 10;
+  }
+
+  return reversed == x;
+}
+
+unsigned int get_largest_palindrome_product() {
+  unsigned int largest_palindrome = 0; 
+  unsigned int prev_smaller = 100;
+
+  for (unsigned int i = 999; i >= 100; --i) {
+
+    // We only have to loop to the smaller number found previously, since the
+    // outer loop number will have decreased
+    for (unsigned int j = i; j >= prev_smaller; --j) {
+      unsigned int tmp = i * j;
+
+      if (is_palindrome(tmp) && tmp > largest_palindrome) {
+        largest_palindrome = tmp;
+        prev_smaller = j;
+      }
+    }
+
+    // We can break as soon as the largest found palindrome is greater than the
+    // square of the outer loop number, as there will be no larger found
+    // palindrome.
+    if (largest_palindrome != 0 && i * i < largest_palindrome)
+      break;
+  }
+
+  return largest_palindrome;
+}
+
+int main(int argc, char** argv) {
+  std::cout << get_largest_palindrome_product() << std::endl;
+}
