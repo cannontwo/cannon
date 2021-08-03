f17e6f61bfca4e1e4a0d5828210963ec01659636
Cannon Lewis <cannon10100@gmail.com>
2021-08-03 12:47:09 -0500

    Documented plot/polygon.hpp (#86)

--- cannon/plot/polygon.hpp
+++ cannon/plot/polygon.hpp
@@ -1,6 +1,11 @@
 #ifndef CANNON_PLOT_POLYGON_H
 #define CANNON_PLOT_POLYGON_H 
 
+/*!
+ * \file cannon/plot/polygon.hpp
+ * \brief File containing Polygon class definition for plotting.
+ */
+
 #include <Eigen/Dense>
 
 #include <cannon/utils/class_forward.hpp>
@@ -19,32 +24,52 @@ namespace cannon {
 
   namespace plot {
 
+    /*!
+     * \brief Class representing a polygon to be plotted by a Plotter. Used for
+     * general surface plotting.
+     */
     class Polygon {
       public:
         Polygon() = delete;
 
+        /*!
+         * \brief Constructor taking a shader for drawing, a vector of vertices
+         * for the polygon, and a matrix of colors for each vertex. Each row in
+         * the matrix is interpreted as a separate vertex color.
+         */
         Polygon(ShaderProgramPtr program, const std::vector<Vector2d>& poly, const
             MatrixX4f& colors);
 
+        /*!
+         * \brief Constructor taking a shader for drawing, a vector of vertices
+         * for the polygon, and a color.
+         */
         Polygon(ShaderProgramPtr program, const std::vector<Vector2d>& poly, const
             Vector4f& color);
 
         Polygon(Polygon& o) = delete;
 
+        /*!
+         * \brief Move constructor.
+         */
         Polygon(Polygon&& o) : points_(o.points_), color_(o.color_),
         vao_(o.vao_), buf_(std::move(o.buf_)), program_(std::move(o.program_)) {}
 
+        /*!
+         * \brief Draw this polygon.
+         */
         void draw();
 
         friend class Plotter;
 
       private:
-        MatrixX2f points_;
-        MatrixX4f color_;
-        VertexArrayObjectPtr vao_;
-        VertexBufferPtr buf_;
-        VertexBufferPtr color_buf_;
-        ShaderProgramPtr program_;
+        MatrixX2f points_; //!< Points defining the vertices of this polygon
+        MatrixX4f color_; //!< Colors to associate with each vertex of this polygon
+
+        VertexArrayObjectPtr vao_; //!< VAO for drawing this polygon
+        VertexBufferPtr buf_; //!< VertexBuffer for drawing this polygon
+        VertexBufferPtr color_buf_; //!< Buffer for storing colors for drawing
+        ShaderProgramPtr program_; //!< Shader used to draw this polygon
 
     };
 
