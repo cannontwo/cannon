02c9094cfd0393eec79d08c606917904143960d1
Cannon Lewis <cannon10100@gmail.com>
2021-08-03 13:39:15 -0500

    Documented plot/scatter.hpp (#86)

--- cannon/plot/scatter.cpp
+++ cannon/plot/scatter.cpp
@@ -4,19 +4,20 @@
 #include <cannon/graphics/shader_program.hpp>
 #include <cannon/graphics/vertex_shader.hpp>
 #include <cannon/graphics/fragment_shader.hpp>
+#include <cannon/graphics/vertex_buffer.hpp>
 #include <cannon/plot/plotter.hpp>
 #include <cannon/graphics/vertex_array_object.hpp>
 
 using namespace cannon::plot;
 using namespace cannon::log;
 
-Scatter::Scatter(Plotter& plotter, ShaderProgramPtr program,
-    MatrixX2f points, Vector4f color, float point_size) :
-    plotter_(plotter), points_(points), color_(color),
-    point_size_(point_size), vao_(new VertexArrayObject), buf_(vao_),
-    program_(program)  {
+Scatter::Scatter(Plotter &plotter, ShaderProgramPtr program, MatrixX2f points,
+                 Vector4f color, float point_size)
+    : plotter_(plotter), points_(points), color_(color),
+      point_size_(point_size), vao_(new VertexArrayObject),
+      buf_(new VertexBuffer(vao_)), program_(program) {
   glEnable(GL_PROGRAM_POINT_SIZE);
-  buf_.buffer(points_);
+  buf_->buffer(points_);
   log_info(buf_);
 }
 
@@ -24,7 +25,7 @@ void Scatter::add_points(MatrixX2f point) {
   points_.conservativeResize(points_.rows() + 1, NoChange);
   points_.row(points_.rows() - 1) = point;
 
-  buf_.buffer(points_);
+  buf_->buffer(points_);
   RowVector2f maxes = points_.colwise().maxCoeff();
   RowVector2f mins = points_.colwise().minCoeff();
   plotter_.set_xlim(mins[0], maxes[0]);
@@ -36,7 +37,7 @@ void Scatter::draw() {
   program_->set_uniform("pointSize", point_size_);
   program_->activate();
 
-  buf_.bind();
+  buf_->bind();
   glDrawArrays(GL_POINTS, 0, points_.rows());
 }
 
--- cannon/plot/scatter.hpp
+++ cannon/plot/scatter.hpp
@@ -1,54 +1,81 @@
 #ifndef CANNON_PLOT_SCATTER_H
 #define CANNON_PLOT_SCATTER_H 
 
+/*!
+ * \file cannon/plot/scatter.hpp
+ * \brief File containing Scatter class definition.
+ */
+
 #include <glad/glad.h>
 #include <Eigen/Dense>
 
-#include <cannon/graphics/vertex_buffer.hpp>
 #include <cannon/utils/class_forward.hpp>
 
 using namespace Eigen;
-
-using namespace cannon::graphics;
 
 namespace cannon {
 
   namespace graphics {
     CANNON_CLASS_FORWARD(ShaderProgram);
     CANNON_CLASS_FORWARD(VertexArrayObject);
+    CANNON_CLASS_FORWARD(VertexBuffer);
   }
+
+  using namespace cannon::graphics;
 
   namespace plot {
 
     CANNON_CLASS_FORWARD(Plotter);
 
+    /*!
+     * \brief Class representing a scatter plot which can be drawn by a
+     * Plotter.
+     */
     class Scatter {
       public:
         Scatter() = delete;
 
+        /*!
+         * \brief Constructor taking a plotter, shader program for drawing,
+         * points in the scatter plot, and color for the points, as well as a
+         * size for the displayed points.
+         */
         Scatter(Plotter& plotter, ShaderProgramPtr program, MatrixX2f points,
             Vector4f color, float point_size);
 
         Scatter(Scatter& s) = delete;
 
+        /*!
+         * \brief Move constructor.
+         */
         Scatter(Scatter&& s) : plotter_(s.plotter_), points_(s.points_), color_(s.color_),
           point_size_(s.point_size_), vao_(s.vao_), buf_(std::move(s.buf_)),
           program_(std::move(s.program_)) {}
 
+        /*!
+         * \brief Add points to this scatter plot.
+         *
+         * \param point The points to add. Each row in the matrix is a separate
+         * point.
+         */
         void add_points(MatrixX2f point);
+
+        /*!
+         * \brief Draw this scatter plot.
+         */
+        void draw();
 
         friend class Plotter;
 
       private:
-        void draw();
+        Plotter& plotter_; //!< Plotter that this scatter plot is a part of
+        MatrixX2f points_; //!< Points to be drawn as part of this scatter plot
+        Vector4f color_; //!< Color to draw points
+        float point_size_; //!< Point size for displayed points
 
-        Plotter& plotter_;
-        MatrixX2f points_;
-        Vector4f color_;
-        float point_size_;
-        VertexArrayObjectPtr vao_;
-        VertexBuffer buf_;
-        ShaderProgramPtr program_;
+        VertexArrayObjectPtr vao_; //!< VAO for drawing this scatter plot
+        VertexBufferPtr buf_; //!< Buffer for points to be drawn
+        ShaderProgramPtr program_; //!< Shader program for drawing scatter plot
     };
 
   } // namespace plot
